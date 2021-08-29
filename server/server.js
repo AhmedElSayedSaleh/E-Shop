3b9f7f452f9990632c810ef40c0f5cd68ec02663
ahmedelsayedsaleh <ahmed0elsayed0saleh@gmail.com>
2021-08-29 06:46:17 +0200

    Add data and node server

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import data from './db.js';
+
+const app = express();
+
+app.get('/api/products', (req, res) => {
+    res.send(data)
+})
+
+app.get('/', (req, res) => {
+    res.send('server is ready');
+});
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`server at http://localhost:${port}`);
+});
