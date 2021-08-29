091691ff4e9c5c931f6187530da9d48a2678ccd4
ahmedelsayedsaleh <ahmed0elsayed0saleh@gmail.com>
2021-08-29 23:53:32 +0200

    Add: -HTTP req to get all data by axios -LoadingBox for spinner -MessageBox for reqs errors

--- client/src/compnents/products/ProductsList.js
+++ client/src/compnents/products/ProductsList.js
@@ -1,20 +1,40 @@
-import React, {useState} from 'react';
-import productsData from '../../data/sample.json';
-
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Product from "./Product";
+import LoadingBox from "../LoadingBox";
+import MessageBox from "../MessageBox";
 
 const ProductsList = () => {
-    const [modalView, setModalView] = useState({});
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                setLoading(true)
+                const {data} = await axios.get('/api/products');
+                setLoading(false)
+                setProducts(data)
+            } catch (err){
+                setError(err.message);
+                setLoading(false)
+            }
+        };
+        fetchData()
+    }, []);
 
+    const [modalView, setModalView] = useState({});
     const modalHandle = (product) => {
-        setModalView(productsData.find(el => el.id === product.id));
+        setModalView(products.find(el => el.id === product.id));
     }
 
     return (
         <>
-            {productsData.map(product => (
-                <Product key={product.id} product={product} modalHandle={modalHandle} modalView={modalView} />
-            ))}
+            {loading ? <LoadingBox></LoadingBox>
+            : error ? <MessageBox>{error}</MessageBox>
+            : products.map(product => (
+                <Product key={product.id} product={product} modalHandle={modalHandle} modalView={modalView}/>))
+            }
         </>
     )
 }
