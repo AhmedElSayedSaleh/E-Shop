c6576506f2754498ed6d3226d8d4690aff62aa88
ahmedelsayedsaleh <ahmed0elsayed0saleh@gmail.com>
2021-12-09 08:05:42 +0200

    deploy to show on live

--- Client/src/components/products/index.js
+++
@@ -1,6 +0,0 @@
-import ColorSelect from "./ColorSelect";
-import SingleProduct from "./SingleProduct";
-import ProductModal from "./ProductModal";
-import Quantity from "./Quantity";
-
-export { ColorSelect, SingleProduct, ProductModal, Quantity };
--- Client/src/layout/index.js
+++
@@ -1,5 +0,0 @@
-import Header from "./Header";
-import Hero from "./Hero";
-import Footer from "./Footer";
-
-export { Header, Hero, Footer };
