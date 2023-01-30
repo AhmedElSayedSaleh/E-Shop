8bababa97f8b6b9a9ec40f3554c089356776e3de
ahmedelsayedsaleh <ahmed0elsayed0saleh@gmail.com>
2023-01-30 23:48:46 +0200

    #Add Login page responsive

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -17,20 +17,18 @@ const Login = () => {
         </div> */}
       </div>
       <div className="row align-items-center flex-column text-center pt-5 px-5 mx-5 mt-5">
-        {/* <div className="d-flex "> */}
-        <div className="w-50">
+        <div className="col">
           <h2 className="m-auto" style={{ fontSize: "2.2rem", width: "70%" }}>
             Log in
           </h2>
         </div>
-        <div className="w-75 d-flex justify-content-center">
+        <div className="col-md-8 col-lg-6">
           <p
-            className="p-5"
+            className="py-5 px-lg-5"
             style={{
               fontSize: "1.3rem",
               lineHeight: "2.5rem",
               color: "#7d7d7d",
-              width: "60%",
             }}
           >
             Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -38,10 +36,9 @@ const Login = () => {
             ever since the 1500s
           </p>
         </div>
-        {/* </div> */}
       </div>
       <div className="row justify-content-center align-items-center">
-        <div className="w-50">
+        <div className="col-12 col-sm-9 col-lg-6 col-xl-5">
           <form>
             <div className="mb-3">
               <Input type={"email"} placeholder={"Email"} />
@@ -49,22 +46,24 @@ const Login = () => {
             <div className="mb-3">
               <Input type={"password"} placeholder={"Password"} />
             </div>
-            <div className="my-4 d-flex justify-content-between">
-              <div className="form-check">
-                <input
-                  type="checkbox"
-                  className="form-check-input"
-                  id="exampleCheck1"
-                />
-                <label
-                  className="form-check-label"
-                  htmlFor="exampleCheck1"
-                  style={{ fontSize: "1.3rem" }}
-                >
-                  Keep me signed in
-                </label>
+            <div className="row my-4">
+              <div className="col-6">
+                <div className="form-check">
+                  <input
+                    type="checkbox"
+                    className="form-check-input"
+                    id="exampleCheck1"
+                  />
+                  <label
+                    className="form-check-label"
+                    htmlFor="exampleCheck1"
+                    style={{ fontSize: "1.3rem" }}
+                  >
+                    Keep me signed in
+                  </label>
+                </div>
               </div>
-              <div>
+              <div className="col-6 text-end">
                 <Link
                   to={"/password"}
                   role="button"
@@ -74,47 +73,59 @@ const Login = () => {
                 </Link>
               </div>
             </div>
-            <div className="d-flex justify-content-between">
-              <Button
-                children={"Facebook"}
-                type={"button"}
-                onClick={(e) => console.log(e)}
-                style={{
-                  width: "100%",
-                  textTransform: "none",
-                  margin: "2rem 1rem 0 0",
-                  backgroundColor: "#3b5998",
-                  color: "#ffffff",
-                  border: "solid 1px #3b5998",
-                }}
-              />
-              <Button
-                children={"Gmail"}
-                type={"button"}
-                onClick={(e) => console.log(e)}
-                style={{
-                  width: "100%",
-                  textTransform: "none",
-                  margin: "2rem 0 0 1rem",
-                  backgroundColor: "#ffffff",
-                  border: "solid 1px #f1584d",
-                }}
-              />
+            <div className="row">
+              <div className="col-7 col-sm-6 col-md-5 mx-auto">
+                <Button
+                  children={"Facebook"}
+                  type={"button"}
+                  onClick={(e) => console.log(e)}
+                  style={{
+                    width: "100%",
+                    textTransform: "none",
+                    margin: "0 0",
+                    backgroundColor: "#3b5998",
+                    color: "#ffffff",
+                    border: "solid 1px #3b5998",
+                  }}
+                />
+              </div>
+              <div className="col-7 col-sm-6 col-md-5 mx-auto pt-3 pt-sm-0">
+                <Button
+                  children={"Gmail"}
+                  type={"button"}
+                  onClick={(e) => console.log(e)}
+                  style={{
+                    width: "100%",
+                    textTransform: "none",
+                    margin: "0 0",
+                    backgroundColor: "#ffffff",
+                    border: "solid 1px #f1584d",
+                  }}
+                />
+              </div>
             </div>
-            <Button
-              children={"Sign in"}
-              type={"submit"}
-              onClick={(e) => console.log(e)}
-              style={{
-                width: "100%",
-                textTransform: "none",
-                margin: "3rem 0 0 0",
-              }}
-            />
+            <div className="row">
+              <div className="col-8 col-sm-6 mx-auto">
+                <Button
+                  children={"Sign in"}
+                  type={"submit"}
+                  onClick={(e) => console.log(e)}
+                  style={{
+                    width: "100%",
+                    textTransform: "none",
+                    margin: "3rem 0 0 0",
+                  }}
+                />
+              </div>
+            </div>
           </form>
           <div className="text-center my-4" style={{ fontSize: "1.3rem" }}>
             <span>Not a member yet?</span>
-            <Link to={"/register"} className="text-black ps-5" role="button">
+            <Link
+              to={"/register"}
+              className="text-black ps-5 text-decoration-underline"
+              role="button"
+            >
               Sign up
             </Link>
           </div>
