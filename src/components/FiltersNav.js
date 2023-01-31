04c43b21a1eb8e98ed3de0feac56c46a939f0944
ahmedelsayedsaleh <ahmed0elsayed0saleh@gmail.com>
2023-01-31 17:50:12 +0200

    #Clear FilterNav comments

--- src/components/FiltersNav.js
+++ src/components/FiltersNav.js
@@ -2,7 +2,6 @@ import React from "react";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import { Icon } from "./";
-// import { setFiltered } from "../store/actions/productActions";
 
 const FiltersNav = ({
   subcategories,
@@ -11,24 +10,6 @@ const FiltersNav = ({
   minPrice,
   maxPrice,
 }) => {
-  // const [prices, setPrices] = useState([]);
-  // const [minPrice, setMinPrice] = useState(0);
-  // const [maxPrice, setMaxPrice] = useState(100);
-
-  // useEffect(() => {
-  // let pricesList = [];
-  // filteredProducts.map((item) => pricesList.push(item.current_price));
-  // setPrices(pricesList);
-  // setMinPrice(Math.ceil(Math.min(...pricesList)));
-  // setMaxPrice(Math.ceil(Math.max(...pricesList)));
-  // }, [filteredProducts, minPrice, maxPrice]);
-
-  // console.log(minPrice, maxPrice);
-
-  // const handleChange = (e) => {
-  //   console.log(e);
-  // };
-
   return (
     <div className="filter-nav">
       <div
@@ -169,62 +150,10 @@ const FiltersNav = ({
                         ]}
                       />
                     </div>
-                    {/* <label htmlFor="minRange" className="form-label">
-                min range
-              </label>
-              <label htmlFor="maxRange" className="form-label">
-                max range
-              </label>
-              <input
-                type="range"
-                className="form-range thumb thumb--zindex-3"
-                id="minRange"
-                min={0}
-                max={100}
-                // value={minVal}
-              />
-              <input
-                type="range"
-                className="form-range thumb thumb--zindex-4"
-                id="maxRange"
-                min={0}
-                max={100}
-                // value={maxVal}
-              /> */}
                   </div>
                 </div>
               </div>
             </div>
-            {/* <div className="accordion-item">
-        <h2 className="accordion-header" id="panelsStayOpen-headingThree">
-          <button
-            className="accordion-button text-uppercase"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#panelsStayOpen-collapseThree"
-            aria-expanded="false"
-            aria-controls="panelsStayOpen-collapseThree"
-          >
-            color
-          </button>
-        </h2>
-        <div
-          id="panelsStayOpen-collapseThree"
-          className="accordion-collapse collapse show"
-          aria-labelledby="panelsStayOpen-headingThree"
-        >
-          <div className="accordion-body">
-            <strong>This is the third item's accordion body.</strong> It is
-            hidden by default, until the collapse plugin adds the appropriate
-            classes that we use to style each element. These classes control the
-            overall appearance, as well as the showing and hiding via CSS
-            transitions. You can modify any of this with custom CSS or
-            overriding our default variables. It's also worth noting that just
-            about any HTML can go within the <code>.accordion-body</code>,
-            though the transition does limit overflow.
-          </div>
-        </div>
-      </div> */}
           </div>
         </div>
       </div>
